2b1a8f8a66dbe94fc2ccfecbf87c6831582ce170
unknown <jafrasele@gmail.com>
2021-09-01 10:17:45 -0500

    Version inicial

---
+++ src/ejemploRectangulo/TestRectangulo.java
@@ -0,0 +1,26 @@
+
+package ejemploRectangulo;
+
+public class TestRectangulo {
+    
+    public static void main(String[] args) {
+        
+        Rectangulo a = new Rectangulo();
+        a.setAltura(20);
+        a.setBase(10);
+        imprimirInfoRectangulo(a);
+        
+        Rectangulo b = new Rectangulo();
+        imprimirInfoRectangulo(b);
+        
+    }
+    
+    public static void imprimirInfoRectangulo(Rectangulo r){
+        System.out.println("Base: " + r.getBase());
+        System.out.println("Altura: "+ r.getAltura());
+        System.out.println("Color: " + Rectangulo.COLOR);
+        System.out.println("Area: " + r.calcularArea());
+        System.out.println("Perimetro"+r.calcularPerimetro());
+        System.out.println("-----------------------------------");
+    }
+}
